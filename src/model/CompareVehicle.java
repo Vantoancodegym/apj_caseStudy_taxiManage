7ba6a3aac9ee6fbdc85bf28d40e5fb68570f6b1a
vantoanftu93 <73921998+vantoanftu93@users.noreply.github.com>
2021-01-15 15:30:33 +0700

    creat interface comparator

---
+++ src/model/CompareVehicle.java
@@ -0,0 +1,10 @@
+package model;
+
+import java.util.Comparator;
+
+public class CompareVehicle implements Comparator<Vehicle> {
+    @Override
+    public int compare(Vehicle o1, Vehicle o2) {
+        return 0;
+    }
+}
--- src/service/VehicleManage.java
+++ src/service/VehicleManage.java
@@ -1,14 +1,22 @@
 package service;
 
+import model.CompareVehicle;
 import model.Vehicle;
 
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 
 public class VehicleManage {
     private List<Vehicle> vehicleList;
+    private Comparator<Vehicle> comparator;
     public VehicleManage(List<Vehicle> vehicleList){
         this.vehicleList=vehicleList;
     }
+    public void setComparator(Comparator<Vehicle> comparator) {
+        this.comparator = comparator;
+    }
+
     public void addNew(Vehicle vehicle){
         vehicleList.add(vehicle);
     }
@@ -26,5 +34,17 @@ public class VehicleManage {
     }
     public List<Vehicle> getNewList(){
 
+    }
+    public void sortAndShow(){
+        List<Vehicle> list=this.getNewList();
+        Collections.sort(list,comparator);
+        for (Vehicle v:list) {
+            System.out.println(v.toString());
+        }
+    }
+    public void showAll(){
+        for (Vehicle v:vehicleList) {
+            System.out.println(v.toString());
+        }
     }
 }
